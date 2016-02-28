aa953fd4cf479242989f7659e800cea73dd67b37
Jeff Schomay <jschomay@gmail.com>
2016-02-28 14:35:21 -0700

    outline heatmap and convert ints to floats

--- heatmap.elm
+++ heatmap.elm
@@ -4,13 +4,13 @@ import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 import Random exposing (..)
 
-randomPoint : Generator (Float,Float)
+randomPoint : Generator (Int, Int)
 randomPoint =
-  pair (float -400 400) (float -300 300)
+  pair (int -300 300) (int -400 400)
 
-randomLevel : Generator Float
+randomLevel : Generator Int
 randomLevel =
-  float 20 300
+  int  20 300
 
 generateSpark: Int -> Form
 generateSpark seed =
@@ -29,15 +29,24 @@ sparksList =
     generateSpark 6
   ]
 
-type alias Point = ( Float, Float )
-spark: Point -> Float -> Form
-spark coords radius =
-  let grad = radial (0,0) 0 (0,0) radius [ (  0.5, rgba 200 0 0 0.5 ) , ( 1, rgba 100 0 0 0 ) ] 
+type alias Point = ( Int, Int )
+spark: Point -> Int -> Form
+spark (x, y) radius =
+  let grad = radial (0,0) 0 (0,0) ( toFloat radius ) [ (  0.5, rgba 200 0 0 0.5 ) , ( 1, rgba 100 0 0 0 ) ] 
   in
-    circle radius
+    circle ( toFloat radius )
       |> gradient grad
-      |> move coords
+      |> move (toFloat x, toFloat y)
+
+blueprint: Int -> Int -> Form
+blueprint w h =
+  rect ( toFloat w ) ( toFloat h )
+    |> outlined ( { defaultLine | width = 2 } )
 
 
 main = 
-    collage 800 600 sparksList
+  let
+      w = 800
+      h = 600
+  in
+     collage w h ( [ blueprint w h ] ++ sparksList )
